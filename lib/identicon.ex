29e991c53711111ade3e88d2b85a11bb91cbb643
mayank1495 <mayank007.win@gmail.com>
2018-08-31 13:18:16 +0530

    Added main hash_input func

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,13 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for Identicon.
-  """
+  def main(input) do
+    input
+    |> hash_input
+  end
 
-  @doc """
-  Hello world.
+  def hash_input(input) do
+    hex = :crypto.hash(:md5,input)
+    |> :binary.bin_to_list
 
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    %Identicon.Image{hex: hex}
   end
 end
