66c9d0a5156f22606faf9420c1c5831618e196fe
mayank1495 <mayank007.win@gmail.com>
2018-08-31 14:05:10 +0530

    Added build_grid func

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,23 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid = 
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first,second | _tail] = row
+    row ++ [second,first]
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
